1ce418ac54f25b230250ed9ec0c9d6483f384a5d
Iranjin <105309719+Iranjin@users.noreply.github.com>
2023-06-08 14:32:32 +0900

    Update TermGecko.py

--- TermGecko.py
+++ TermGecko.py
@@ -1,23 +1,4 @@
-#!/usr/bin/env python
-import contextlib as __stickytape_contextlib
-
-@__stickytape_contextlib.contextmanager
-def __stickytape_temporary_dir():
-    import tempfile
-    import shutil
-    dir_path = tempfile.mkdtemp()
-    try:
-        yield dir_path
-    finally:
-        shutil.rmtree(dir_path)
-
-with __stickytape_temporary_dir() as __stickytape_working_dir:
-    def __stickytape_write_module(path, contents):
-        import os, os.path
-
-        def make_package(path):
-            parts = path.split("/")
-            partial_path = __stickytape_working_dir
+pe_working_dir
             for part in parts:
                 partial_path = os.path.join(partial_path, part)
                 if not os.path.exists(partial_path):
